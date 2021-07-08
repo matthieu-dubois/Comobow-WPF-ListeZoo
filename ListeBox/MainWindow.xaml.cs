d45ca07e1edb4d793913248d8d6787ee1a099348
matthieu <matthieu.dubois@epsi.fr>
2021-07-08 20:56:14 +0200

    FirstCommit

---
+++ ListeBox/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListeBox
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection sqlConnection;
+
+        public MainWindow()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ListeBox.Properties.Settings.WPFConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+            InitializeComponent();
+            ShowPersonne();
+            ShowAssociatedOrders();
+        }
+
+        private void ShowPersonne()
+        {
+
+            try
+            {
+                string query = "select * from Persons";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable PersonTable = new DataTable();
+                    sqlDataAdapter.Fill(PersonTable);
+
+                    listePersonne.DisplayMemberPath = "LastName";
+                    listePersonne.SelectedValuePath = "id";
+                    listePersonne.ItemsSource = PersonTable.DefaultView;
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+
+        private void ShowAssociatedOrders()
+        {
+
+            try
+            {
+                string query = "select * from Orders o inner join Persons p on o.PersonID = p.ID where p.ID = @id";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    
+                    sqlCommand.Parameters.AddWithValue("@id", listePersonne.SelectedValue);
+
+                    DataTable PersonTable = new DataTable();
+
+                    sqlDataAdapter.Fill(PersonTable);
+
+                    OrderBypersonne.DisplayMemberPath = "OrderNumber";
+                    OrderBypersonne.ItemsSource = PersonTable.DefaultView;
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+
+        private void listePersonne_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ShowAssociatedOrders();
+        }
+    }
+}
---
+++ ListeZoo/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListeZoo
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection sqlConnection;
+        public MainWindow()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ListeZoo.Properties.Settings.WPFDBConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+            InitializeComponent();
+            ShowZoos();
+        }
+
+
+        private void ShowZoos()
+        {
+            try
+            {
+                string query = "select * from Zoo";
+                // the SqlDataAdapter can be imagined like an Interface to make Tables usable by C#-Objects
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable zooTable = new DataTable();
+
+                    sqlDataAdapter.Fill(zooTable);
+
+                    //Which Information of the Table in DataTable should be shown in our ListBox?
+                    ListeZoo.DisplayMemberPath = "Location";
+                    //Which Value should be delivered, when an Item from our ListBox is selected?
+                    ListeZoo.SelectedValuePath = "Id";
+                    //The Reference to the Data the ListBox should populate
+                    ListeZoo.ItemsSource = zooTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+        }
+
+
+        private void ShowAssociatedAnimals()
+        {
+            try
+            {
+                string query = "select * from Animal a inner join ZooAnimal " +
+                    "za on a.Id = za.AnimalId where za.ZooId = @ZooId";
+
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                // the SqlDataAdapter can be imagined like an Interface to make Tables usable by C#-Objects
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+                using (sqlDataAdapter)
+                {
+
+                    sqlCommand.Parameters.AddWithValue("@ZooId", ListeZoo.SelectedValue);
+
+                    DataTable animalTable = new DataTable();
+
+                    sqlDataAdapter.Fill(animalTable);
+
+                    //Which Information of the Table in DataTable should be shown in our ListBox?
+                    listeAnimal.DisplayMemberPath = "Name";
+                    //Which Value should be delivered, when an Item from our ListBox is selected?
+                    listeAnimal.SelectedValuePath = "Id";
+                    //The Reference to the Data the ListBox should populate
+                    listeAnimal.ItemsSource = animalTable.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+                // MessageBox.Show(e.ToString());
+            }
+
+        }
+
+        private void ListeZoo_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            ShowAssociatedAnimals();
+        }
+    }
+}
